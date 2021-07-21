8c0bf0dcc7025b5ce61ee09d910f47e4f615f7ca
Mandar <mandy64theking@gmail.com>
2021-07-21 10:48:01 +0530

    changed the file for appropriation with use

--- scraperflip.py
+++ scraperflip.py
@@ -2,33 +2,33 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 
-URL = input("Enter URL : ")
-budget = float(input("Enter ALert Budget : "))
-toemail = input("Enter your email ID : ")
+# URL = input("Enter URL : ")
+# budget = float(input("Enter ALert Budget : "))
+# toemail = input("Enter your email ID : ")
 headers = {
     "User-Agent":
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.16 Safari/537.36 Edg/80.0.361.9'
 }
 
 
-def check_price():
+def check_price(URL,budget,toemail):
     page = requests.get(URL, headers=headers)
 
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    title = soup.find("span", {"class": "_35KyD6"}).get_text()
-    price = soup.find("div", {"class": "_1vC4OE _3qQ9m1"}).get_text()
+    title = soup.find("span", {"class": "B_NuCI"}).get_text()
+    price = soup.find("div", {"class": "_30jeq3 _16Jk6d"}).get_text()
 
     new_price = price[1:]
     array_price = new_price.split(',')
     converted_price = float(array_price[0] + array_price[1])
     if (converted_price < budget):
-        send_mail()
+        send_mail(toemail,URL)
     print(converted_price)
     print(title)
 
 
-def send_mail():
+def send_mail(toemail,URL):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
@@ -45,4 +45,4 @@ def send_mail():
     server.quit()
 
 
-check_price()
+# check_price()
